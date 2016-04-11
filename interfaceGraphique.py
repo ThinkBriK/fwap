26902b8b61ce02d7a3af4ed6fa771480ff198701
thinkbrik <thnkbrik@vm-herder.net>
2016-04-11 16:00:12 +0200

    Mise en place d'une interface basée sur un notebook

--- interfaceGraphique.py
+++ interfaceGraphique.py
@@ -1,18 +1,7 @@
 import tkinter as Tk
+from tkinter import ttk
 
 import FWAP
-
-
-########################################################################
-class OtherFrame(Tk.Toplevel):
-    """"""
-
-    # ----------------------------------------------------------------------
-    def __init__(self):
-        """Constructor"""
-        Tk.Toplevel.__init__(self)
-        self.geometry("400x300")
-        self.title("otherFrame")
 
 
 ########################################################################
@@ -25,29 +14,141 @@ class MyApp(object):
         self.root = parent
         self.root.title("Déploiement TAT1")
         self.frame = Tk.Frame(parent)
-        self.frame.pack()
+        self.frame.grid()
         self.fwapfile = FWAP.FwapFile(fwapfile)
-        self.display_xml_window()
+        self._create_widgets()
+
+    def _create_widgets(self):
+        # create the notebook
+        nb = ttk.Notebook(self.frame, name='notebook')
+
+        # extend bindings to top level window allowing
+        #   CTRL+TAB - cycles thru tabs
+        #   SHIFT+CTRL+TAB - previous tab
+        #   ALT+K - select tab using mnemonic (K = underlined letter)
+        nb.enable_traversal()
+        nb.grid(padx=2, pady=3)
+        self._create_fwap_tab(nb)
+        self._create_vcenter_tab(nb)
+        self._create_request_tab(nb)
 
     # ----------------------------------------------------------------------
-    def hide(self):
-        """"""
-        self.root.withdraw()
+    def onFwapSelect(self, tree):
+        choix = tree.focus()
+
+        # On vérifie qu'on soit bien sur un serveur (feuille)
+        if len(tree.get_children(choix)) == 0:
+            self.servername = tree.item(choix)['text']
+            print("servername = " + self.servername)
+
+            rds = tree.parent(choix)
+            self.rds = tree.item(rds)['text']
+            print("RDS = " + self.rds)
+
+            ep = tree.parent(rds)
+            self.ep = tree.item(ep)['text']
+            print("EP = " + self.ep)
+
+    def _create_fwap_tab(self, notebook):
+        frame = ttk.Frame(notebook, name='fwap')
+        tree = self.fwapfile.get_tk_tree(parent=frame, label="Choisissez un serveur", type='serveur')
+        # tree.bind('<<TreeviewSelect>>', self.fwap_select)
+        tree.grid(row=0, column=0)
+
+        handler = lambda: self.onFwapSelect(tree)
+        boutonOK = Tk.Button(frame, text="OK", command=handler)
+        boutonOK.grid(row=0, column=1, sticky='SE')
+
+        # add to notebook (underline = index for short-cut character)
+        notebook.add(frame, text='FWAP', padding=2)
 
     # ----------------------------------------------------------------------
-    def openFrame(self):
+    def _create_vcenter_tab(self, notebook):
         """"""
-        self.hide()
-        subFrame = OtherFrame()
-        handler = lambda: self.onCloseOtherFrame(subFrame)
-        btn = Tk.Button(subFrame, text="Close", command=handler)
-        btn.pack()
+        frame = ttk.Frame(notebook, name='vcenter')
+
+        handler = lambda: self.onUpdateParams(
+            params_dict={'vcenter': vcenter.get(), 'password': passwd.get(), 'user': usr.get()})
+
+        label_vcenter = Tk.Label(frame, text="vCenter")
+        label_vcenter.grid(row=0, column=0, sticky='W')
+        vcenter = ttk.Combobox(frame, values=("a82avce02.agora.msanet", "a82avce96.agora.msanet"), width=25)
+        vcenter.set("a82avce02.agora.msanet")
+        vcenter.grid(row=0, column=1, sticky='W')
+
+        label_usr = Tk.Label(frame, text="User vCenter")
+        label_usr.grid(row=1, column=0, sticky='W')
+        usr = Tk.Entry(frame, width=25)
+        usr.grid(row=1, column=1, sticky='W')
+
+        label_pwd = Tk.Label(frame, text="Password vCenter")
+        label_pwd.grid(row=2, column=0, sticky='W')
+        passwd = Tk.Entry(frame, show="*", width=25)
+        passwd.grid(row=2, column=1, sticky='W')
+
+        btn = Tk.Button(frame, text="OK", command=handler)
+        btn.grid(row=3, column=1, sticky='W')
+
+        notebook.add(frame, text='Connexion vcenter', padding=2)
+
+        # ----------------------------------------------------------------------
 
     # ----------------------------------------------------------------------
-    def onCloseOtherFrame(self, otherFrame):
+    def _create_request_tab(self, notebook):
+        # demandeur, fonction, eol
+
+        frame = ttk.Frame(notebook, name='request')
+        handler = lambda: self.onUpdateParams(
+            params_dict={'demandeur': demandeur.get(), 'fonction': fonction.get(), 'eol': eol.get(),
+                         'vcpus': vcpus.get(), 'ram': ram.get()})
+
+        label_demandeur = Tk.Label(frame, text="Demandeur")
+        label_demandeur.grid(row=0, column=0, sticky='W')
+        demandeur = Tk.Entry(frame, width=30)
+        demandeur.grid(row=0, column=1, sticky='W')
+
+        label_fonction = Tk.Label(frame, text="Fonction")
+        label_fonction.grid(row=1, column=0, sticky='W')
+        fonction = Tk.Entry(frame, width=30)
+        fonction.grid(row=1, column=1, sticky='W')
+
+        label_eol = Tk.Label(frame, text="Fin de vie")
+        label_eol.grid(row=2, column=0, sticky='W')
+        eol = Tk.Entry(frame, width=30)
+        eol.value = "Perenne"
+        eol.grid(row=2, column=1, sticky='W')
+
+        sep = ttk.Separator(orient='horizontal')
+        sep.grid(row=3, column=0, sticky='W')
+
+        label_vcpu = Tk.Label(frame, text="Entrez le nombre de vCPUs")
+        label_vcpu.grid(row=4, column=0, sticky='W')
+        vcpus = Tk.Spinbox(frame, from_=1, to=12)
+        vcpus.value = 1
+        vcpus.grid(row=4, column=1, sticky='W')
+
+        label_ram = Tk.Label(frame, text="RAM en GB")
+        label_ram.grid(row=5, column=0, sticky='W')
+        ram = Tk.Spinbox(frame, from_=1, to=64)
+        ram.value = 1
+        ram.grid(row=5, column=1, sticky='W')
+
+        btn = Tk.Button(frame, text="OK", command=handler)
+        btn.grid(row=6, column=1, sticky='W')
+
+        notebook.add(frame, text='Demande', padding=2)
+
+        # datacenter, cluster, esx,
+
+        # vmfolder, lan dépendent de l'ESX sélectionné
+
+    # ----------------------------------------------------------------------
+    def onUpdateParams(self, params_dict):
         """"""
-        otherFrame.destroy()
-        self.show()
+        for key in params_dict.keys():
+            setattr(self, key, params_dict[key])
+
+        print(self.__dict__)
 
     # ----------------------------------------------------------------------
     def show(self):
@@ -55,32 +156,7 @@ class MyApp(object):
         self.root.update()
         self.root.deiconify()
 
-    # ----------------------------------------------------------------------
-    def display_xml_window(self):
-        fenetre = self.frame
-        tree = self.fwapfile.get_tk_tree(fenetre, label="Choisissez un serveur", type='serveur')
-        tree.pack()
 
-        # bouton de sortie
-        boutonFermer = Tk.Button(fenetre, text="Fermer", command=fenetre.quit)
-        boutonFermer.pack(side='left')
-        boutonOK = Tk.Button(fenetre, text="OK", command=fenetre.quit)
-        boutonOK.pack(side='right')
-        fenetre.mainloop()
-        choix = tree.focus()
-
-        # TODO Manquent vcenter, user, password, vcpu, ram, lan, datacenter, datastore, cluster, esx, vmfolder, demandeur, fonction, eol,
-        # TODO passer ovfpath en conf
-
-
-        servername = tree.item(choix)['text']
-        print("servername = " + servername)
-
-        rds = tree.parent(choix)
-        print("RDS = " + tree.item(rds)['text'])
-
-        ep = tree.parent(rds)
-        print("EP = " + tree.item(ep)['text'])
 
 
 # ----------------------------------------------------------------------
@@ -89,3 +165,5 @@ if __name__ == "__main__":
     root.geometry()
     app = MyApp(root, 'files/FWAP.xml')
     root.mainloop()
+    # TODO Rajouter vcpu, ram, lan, datacenter, datastore, cluster, esx, vmfolder,
+    # TODO Passer ovfpath en params
